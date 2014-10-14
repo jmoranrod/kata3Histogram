68c4945210b179f24d7698f0388c50ccc333389a
Javi <Javi@10.230.168.71>
2014-10-14 14:19:00 +0100

    More methods implemented.

--- src/kata3histogram/Kata3Histogram.java
+++ src/kata3histogram/Kata3Histogram.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata3histogram;
 
-/**
- *
- * @author Javi
- */
 public class Kata3Histogram {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MailReader reader = new MailReader("mails.txt");
+        HistogramBuilder<String> builder = new HistogramBuilder<>();
+        Histogram<String> histogram = builder.build(reader.readDomains());
+        new HistogramViewer<String>().print(histogram);
     }
     
 }
--- src/kata3histogram/MailReader.java
+++ src/kata3histogram/MailReader.java
@@ -1,5 +1,11 @@
 package kata3histogram;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MailReader {
 
     private final String filePath;
@@ -8,6 +14,19 @@ public class MailReader {
         this.filePath = filePath;
     }
     
-    
+    public String[] readDomains() {
+        ArrayList<String> domainList = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            while (true) {                
+                String line = reader.readLine();
+                if(line == null) break;
+                if(line.contains("@"))
+                    domainList.add(line.split("@")[1]);
+            }
+        } catch (IOException ex) {
+        }
+        return domainList.toArray(new String[domainList.size()]);
+    }
     
 }
