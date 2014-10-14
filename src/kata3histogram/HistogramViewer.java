2ad450a75081348c1d893b083482666649966ac2
Javi <Javi@10.230.168.71>
2014-10-14 13:30:08 +0100

    HistogramViewer class created and implemented.

---
+++ src/kata3histogram/HistogramViewer.java
@@ -0,0 +1,11 @@
+package kata3histogram;
+
+public class HistogramViewer<Type>{
+    
+    public void print(Histogram<Type> histogram){
+        for (Type type : histogram.keySet()) {
+            System.out.println(type + "->" + histogram.get(type));
+        }
+    }
+    
+}
