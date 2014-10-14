e60ba84b36954d4b0a8047b5c436b9eff186affb
Javi <Javi@10.230.168.71>
2014-10-14 13:23:16 +0100

    "get" method redefined.

--- src/kata3histogram/Histogram.java
+++ src/kata3histogram/Histogram.java
@@ -3,5 +3,14 @@ package kata3histogram;
 import java.util.HashMap;
 
 public class Histogram <Type> extends HashMap<Type, Integer>{
+
+    @Override
+    public Integer get(Object key) {
+        if (containsKey(key))
+            return super.get(key);
+        return 0;
+    }
+    
+    
     
 }
