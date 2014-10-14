18efc3dd57db54f9e0d9538c009b591f2cb5fb06
Javi <Javi@10.230.168.71>
2014-10-14 13:12:17 +0100

    Project creation.

---
+++ src/kata3histogram/Histogram.java
@@ -0,0 +1,7 @@
+package kata3histogram;
+
+import java.util.HashMap;
+
+public class Histogram <Type> extends HashMap<Type, Integer>{
+    
+}
---
+++ src/kata3histogram/HistogramBuilder.java
@@ -0,0 +1,8 @@
+package kata3histogram;
+
+public class HistogramBuilder <Type> {
+    
+    public Histogram<Type> build(Type[] types){
+        return null;
+    }
+}
